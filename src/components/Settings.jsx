08e412e328f5b2ea138fa50636c0545c05e774f6
francis-asdf <114354354+francis-asdf@users.noreply.github.com>
2025-07-12 13:07:16 -0700

    added dark mode

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,14 +1,25 @@
-function DarkMode() {
+import { useState, useEffect } from 'react'
+import '../index.css'
+
+function DarkMode({ isDarkMode, toggleDarkMode }) {
     return (
-        <button onClick={() => { }}>Dark mode</button>
+        <button onClick={toggleDarkMode}>
+            {isDarkMode ? "Light mode" : "Dark mode"}
+        </button>
     )
 }
 
 export default function Settings() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        document.body.className = isDarkMode ? "dark" : "light";
+    }, [isDarkMode]); // changes body's class name, which is reflected in change in background-color and color
+
     return (
-        <div>
+        <div className={isDarkMode ? "dark" : "light"}>
             <h2>Settings</h2>
-            <DarkMode />
+            <DarkMode isDarkMode={isDarkMode} toggleDarkMode={() => setIsDarkMode(!isDarkMode)} />
         </div>
     )
 }
