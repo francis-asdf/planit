2fca8449282a71f9b7e1db29ef7facad937f3b00
francis-asdf <114354354+francis-asdf@users.noreply.github.com>
2025-08-01 17:08:20 -0700

    styled color of progress bar background

--- src/components/header/LevelProgressBar.jsx
+++ src/components/header/LevelProgressBar.jsx
@@ -8,7 +8,7 @@ export default function LevelProgressBar({ currentPoints, level, levelPoints }) 
         <div className="level-progress-wrapper">
             <div className="level-info">
                 <span className="level-text">Level {level}</span>
-                <span className="points-text">{remainingPoints} points until next level</span>
+                <span className="points-text">{remainingPoints} point{remainingPoints === 1 ? '' : 's'} until next level</span>
             </div>
             <div className="progress-container">
                 <div className="progress-fill" style={{ width: `${percentage}%` }}></div>
